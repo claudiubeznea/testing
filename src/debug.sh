438dd7afd497d8ae79481082883d88bac3c63917
Claudiu Beznea <claudiu.beznea@microchip.com>
2019-08-13 10:59:29 +0300

    src: debug: document logging levels
    Signed-off-by: Claudiu Beznea <claudiu.beznea@microchip.com>

--- src/debug.sh
+++ src/debug.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+# helpers for logging levels
 BOLD='\033[1m'
 GREEN='\033[0;32m'
 RED='\033[0;31m'
 YELLOW='\033[1;33m'
 NC='\033[0m'
 
+# logging levels
 info=${GREEN}${BOLD}
 warn=${YELLOW}${BOLD}
 err=${RED}${BOLD}
