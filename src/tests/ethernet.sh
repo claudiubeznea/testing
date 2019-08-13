eefcbfc63c1cfa84f9f66a0a31a31a203ccdb17b
Claudiu Beznea <claudiu.beznea@microchip.com>
2019-08-13 15:43:17 +0300

    src: tests: ethernet: fix declaration of internal assoc array
    Signed-off-by: Claudiu Beznea <claudiu.beznea@microchip.com>

--- src/tests/ethernet.sh
+++ src/tests/ethernet.sh
@@ -6,7 +6,7 @@ source src/util.sh
 # @args:		none
 # return:		1 - success, 0 - fail
 function testEthernet() {
-	eval "declare -A cfg"=${1#*=}
+	eval "declare -A cfg="${1#*=}
 
 	runCmd "ping -c 1 ${cfg["ip"]}" > /dev/null
 	if [ $? -ne 0 ]; then
