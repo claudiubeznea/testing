42e7a6d34e7290e64e99833cfe5dc27ef0cd253b
Claudiu Beznea <claudiu.beznea@microchip.com>
2019-08-14 16:39:08 +0300

    src: tests: ethernet: update ethernet test
    Add iperf3 tx and rx test.
    
    Signed-off-by: Claudiu Beznea <claudiu.beznea@microchip.com>

--- src/tests/ethernet.sh
+++ src/tests/ethernet.sh
@@ -8,10 +8,67 @@ source src/util.sh
 function testEthernet() {
 	eval "declare -A cfg="${1#*=}
 
-	runCmd "ping -c 1 ${cfg["ip"]}" > /dev/null
+	output=$(runCmd "ping -c 1 ${cfg["ip"]}" y > /dev/null 2>&1)
 	if [ $? -ne 0 ]; then
 		return 0
 	fi
+
+	# RX
+	runCmd "iperf3 -s" >/dev/null 2>&1
+	if [ $? -ne 0 ]; then
+		return 0
+	fi
+
+	$(iperf3 -c ${cfg["ip"]} < /dev/null > /dev/null  2>&1)
+	if [ $? -ne 0 ]; then
+		return 0
+	fi
+
+	rxBw=$(cat /tmp/${cfg["session-id"]}-${cfg["board"]}.log | grep "receiver" | awk '{print $7}')
+	rxBwUnit=$(cat /tmp/${cfg["session-id"]}-${cfg["board"]}.log | grep "receiver" | awk '{print $8}')
+
+	printlog ${info} "RX: ${rxBw} ${rxBwUnit} " y
+	runCmd "pkill -f iperf3" y > /dev/null 2>&1
+	if [ $? -ne 0 ]; then
+		return 0
+	fi
+
+	$(rm -rf /tmp/${cfg["session-id"]}-${cfg["board"]}.log)
+	if [ $? -ne 0 ]; then
+		return 0
+	fi
+
+	# TX
+	$(iperf3 -s < /dev/null > /tmp/detached_local.log 2>&1 &)
+	if [ $? -ne 0 ]; then
+		return 0
+	fi
+	runCmd "iperf3 -c ${cfg["host-ip"]}" y y >/dev/null 2>&1
+	if [ $? -ne 0 ]; then
+		return 0
+	fi
+
+	txBw=$(cat /tmp/${cfg["session-id"]}-${cfg["board"]}.log | grep "sender" | awk '{print $7}')
+	txBwUnit=$(cat /tmp/${cfg["session-id"]}-${cfg["board"]}.log | grep "sender" | awk '{print $8}')
+
+	printlog ${info} "TX: ${txBw} ${txBwUnit} " y
+
+	$(pkill iperf3 > /dev/null 2>&1)
+	if [ $? -ne 0 ]; then
+		return 0
+	fi
+
+	$(rm -rf /tmp/${cfg["session-id"]}-${cfg["board"]}.log)
+	if [ $? -ne 0 ]; then
+		return 0
+	fi
+
+	# TX/RX
+#	runCmd "iperf3 -s" >/dev/null 2>&1
+#	$(iperf3 -s < /dev/null > /tmp/detached_local.log 2>&1 &)
+
+#	outputTarget=$(runCmd "iperf3 -c ${cfg["host-ip"]} -O 5 -t 60" y)
+#	outputHost=$(iperf3 -c ${cfg["ip"]} -O 5 -t 60)
 
 	return 1
 }
