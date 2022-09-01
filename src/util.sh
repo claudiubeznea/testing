78e8f2dc2862d86b8584f10a7e584ed25b5d14e6
Claudiu Beznea <claudiu.beznea@microchip.com>
2022-09-01 17:21:17 +0300

    util: simplify runCmd
    Signed-off-by: Claudiu Beznea <claudiu.beznea@microchip.com>

--- src/util.sh
+++ src/util.sh
@@ -21,56 +21,18 @@ function timeOut() {
 	echo ""
 }
 
-# runCmd:		execute a bash command on remote target
-# @$1:			command to be executed
-# @$2:			block on execution
-# @$3:			log everything to file (need to be cleared after execution)
-# return:		command output
 function runCmd() {
-	local cmd=$1
-	local timeOut=3600
-	local blocking=y
-	local logtofile=n
+	local cmd="$1"
 
-	if [[ ! -z $2 ]]; then
-		timeOut=$2
-	fi
+	output=$(sshpass -p ${config["passwd"]} ssh -o StrictHostKeyChecking=no \
+		 root@${config["ip"]} ${cmd})
 
-	if [[ ! -z $3 ]]; then
-		blocking=y
-	fi
+	# give peace a chance
+	sleep 1
 
-	if [[ ! -z $4 ]]; then
-		logtofile=y
-	fi
+	printf "%s" "${output}"
+}
 
-	if [ "${blocking}" == "y" ]; then
-		if [ "${logtofile}" == "y" ]; then
-			output=$(sshpass -f <(printf '%s\n' ${config["passwd"]}) \
-				timeout ${timeOut} ssh -o StrictHostKeyChecking=no \
-				root@${config["ip"]} ${cmd} > \
-				/tmp/${config["session-id"]}-${config["board"]}.log)
-		else
-			output=$(sshpass -f <(printf '%s\n' ${config["passwd"]}) \
-				timeout ${timeOut} ssh -o StrictHostKeyChecking=no \
-				root@${config["ip"]} ${cmd})
-		fi
-	else
-		if [ "${logtofile}" == "y" ]; then
-			$(sshpass -f <(printf '%s\n' ${config["passwd"]}) timeout \
-			  ${timeOut} ssh -f -o StrictHostKeyChecking=no \
-			  root@${config["ip"]} ${cmd} < /dev/null > \
-			  /tmp/${config["session-id"]}-${config["board"]}.log 2>&1 &)
-		else
-			$(sshpass -f <(printf '%s\n' ${config["passwd"]}) timeout \
-			  ${timeOut} ssh -f -o StrictHostKeyChecking=no \
-			  root@${config["ip"]} ${cmd} < /dev/null > /dev/null 2>&1 &)
-		fi
 
-		# give it a chance to start on remote system
-		sleep 5
-	fi
-
-	echo "${output}"
 }
 
